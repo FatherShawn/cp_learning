da6b550da2cc3abda8ce1bd7b0ec8d05d7237b92
Shawn P. Duncan <sduncan@digitalpulp.com>
2021-11-06 17:12:30 -0400

    structure correction

--- autoencoder.py
+++ autoencoder.py
@@ -1,13 +1,11 @@
 import os
 import torch
 from torch import nn
-
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, random_split
-from torchvision import transforms
 import pytorch_lightning as pl
 
-class GruBlock():
+def gru_block(input_dim, encoded_dim) -> nn.Sequential:
     pass
 
 
