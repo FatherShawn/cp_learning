4c4131ea27afd50c1501a821afa2d8c88165cf95
Shawn P. Duncan <code@sd.shawnduncan.org>
2021-11-06 15:32:25 -0400

    Pytorch lighting structure

---
+++ autoencoder.py
@@ -0,0 +1,25 @@
+import os
+import torch
+from torch import nn
+
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+import pytorch_lightning as pl
+
+class GruBlock():
+    pass
+
+
+class QuackAutoencoder(pl.LightningModule):
+    def __init__(self):
+        pass
+
+    def forward(self, *args, **kwargs) -> Any:
+        pass
+
+    def training_step(self, *args, **kwargs) -> STEP_OUTPUT:
+        pass
+
+    def configure_optimizers(self):
+        pass
