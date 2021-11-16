a63feba0a3e879b4e600dea58bae3fe9e62e715d
Shawn P. Duncan <sduncan@digitalpulp.com>
2021-11-16 07:30:27 -0500

    refactor padding

--- cp_tokenized_data.py
+++ cp_tokenized_data.py
@@ -2,37 +2,43 @@ import torch as pt
 import pytorch_lightning as pl
 import numpy as np
 from pytorch_lightning.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
-from torch.utils.data import DataLoader
-from torch.nn.utils.rnn import PackedSequence, pad_sequence, pack_padded_sequence
+from torch.utils.data import DataLoader, random_split
+from torch.nn import functional as F
 from cp_dataset import QuackIterableDataset
 from typing import Optional
 
 
-def pad_and_pack(batch: list[pt.Tensor]) -> PackedSequence:
-    lengths = pt.from_numpy(np.fromiter((item.size(0) for item in batch), int))
-    batch_padded = pad_sequence(batch, batch_first=True)
-    sequence = pack_padded_sequence(batch_padded, lengths=lengths, batch_first=True, enforce_sorted=False)
-    return sequence
+def pad_right(batch: list[pt.Tensor]) -> pt.Tensor:
+    lengths = np.fromiter((item.size(0) for item in batch), int)
+    max_length = np.max(lengths)
+    batch_padded = [F.pad(item, (0, max_length - item.size(0)), value=0) for item in batch]
+    return pt.stack(batch_padded)
 
 
 class QuackTokenizedDataModule(pl.LightningDataModule):
-    def __init__(self, train_paths: list, test_paths: list, batch_size: int = 64, train_transforms=None,
+    def __init__(self, train_paths: list, validation_paths: list, batch_size: int = 64, train_transforms=None,
                  val_transforms=None, test_transforms=None, dims=None):
         super().__init__(train_transforms, val_transforms, test_transforms, dims)
         self.__train_paths = train_paths
-        self.__test_paths = test_paths
+        self.__val_paths = validation_paths
         self.__batch_size = batch_size
-        self.__train_data = QuackIterableDataset(self.__train_paths, tensors=True)
+        dataset = QuackIterableDataset(self.__train_paths, tensors=True)
+        self.__train_data, self.__test_data = random_split(dataset, [int(len(dataset)*0.8), int(len(dataset)*0.2)])
         width = self.__train_data.data_width()
-        self.__test_data = QuackIterableDataset(self.__test_paths, tensors=True)
+        self.__val_data = QuackIterableDataset(self.__val_paths, tensors=True)
         if self.__test_data.data_width() > width:
             width = self.__test_data.data_width()
         self._dims = (width,)
 
-
-
     def train_dataloader(self) -> TRAIN_DATALOADERS:
-        return DataLoader(self.__train_data, batch_size=self.__batch_size, collate_fn=pad_and_pack, num_workers=4)
+        return DataLoader(self.__train_data, batch_size=self.__batch_size, collate_fn=pad_right, num_workers=4)
 
     def test_dataloader(self) -> EVAL_DATALOADERS:
-        return DataLoader(self.__test_data, batch_size=self.__batch_size, collate_fn=pad_and_pack, num_workers=4)
+        return DataLoader(self.__test_data, batch_size=self.__batch_size, collate_fn=pad_right, num_workers=4)
+
+    def val_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.__val_data, batch_size=self.__batch_size, collate_fn=pad_right, num_workers=4)
+
+    def predict_dataloader(self) -> EVAL_DATALOADERS:
+        pass
+
