cb6582dd850018da62220126ef28c4af91efe0e6
Shawn P. Duncan <sduncan@digitalpulp.com>
2021-12-04 13:49:55 -0500

    Drop back to batch size 8 as my GPU does not support 16bit

--- ae_processor.py
+++ ae_processor.py
@@ -14,11 +14,11 @@ def main() -> None:
         '/data/labeled/2021-08-16-labeled.hdf5',
         '/data/labeled/2021-08-25-labeled.hdf5'
     ]
-    data = QuackTokenizedDataModule(data_paths, batch_size=64)
+    data = QuackTokenizedDataModule(data_paths, batch_size=8)
     # Max time difference determined by data analysis.
     max_index = 131300 + QuackConstants.VOCAB.value
     model = QuackAutoEncoder(num_embeddings=max_index, embed_size=32, hidden_size=128, max_decode_length=data.get_width())
-    trainer = Trainer(gpus=1, precision=16, limit_train_batches=0.1)
+    trainer = Trainer(gpus=1, limit_train_batches=0.1)
     trainer.fit(model, datamodule=data)
 
 
