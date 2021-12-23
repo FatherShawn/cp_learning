5e12956465823f1c34fc2802cd394af2fa06bf3e
Shawn P. Duncan <code@sd.shawnduncan.org>
2021-12-23 06:14:38 -0500

    Move time from milliseconds to seconds to reduce the scale of the model.  Likely a classic case of premature optimization.

--- ae_processor.py
+++ ae_processor.py
@@ -20,7 +20,7 @@ def main() -> None:
     args = arg_parser.parse_args()
     data = QuackTokenizedDataModule(args.data_dir, batch_size=args.batch_size, workers=args.num_workers)
     # Max time difference determined by data analysis.
-    max_index = 131300 + QuackConstants.VOCAB.value
+    max_index = 132 + QuackConstants.VOCAB.value
     model = QuackAutoEncoder(num_embeddings=max_index, embed_size=args.embed_size, hidden_size=args.hidden_size, max_decode_length=data.get_width())
     if args.tune:
         trainer = Trainer.from_argparse_args(args, precision=16, auto_scale_batch_size=True)
