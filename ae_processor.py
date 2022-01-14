eacdca592202e149ce72a3e70b17274a9ef408f0
Shawn P. Duncan <code@sd.shawnduncan.org>
2022-01-14 07:50:03 -0500

    Code adjustments for computing cluster

--- ae_processor.py
+++ ae_processor.py
@@ -3,22 +3,10 @@ from cp_tokenized_data import QuackTokenizedDataModule
 from autoencoder import QuackAutoEncoder
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
 
-def main() -> None:
-    # Add args to make a more flexible cli tool.
-    arg_parser = ArgumentParser()
-    arg_parser.add_argument('--data_dir', type=str, default='/data')
-    arg_parser.add_argument('--batch_size', type=int, default=4)
-    arg_parser.add_argument('--num_workers', type=int, default=0)
-    arg_parser.add_argument('--embed_size', type=int, default=128)
-    arg_parser.add_argument('--hidden_size', type=int, default=512)
-    arg_parser.add_argument('--tune', action='store_true', default=False)
-    arg_parser.add_argument('--checkpoint_path', type=str)
-    # add trainer args (gpus=x, precision=...)
-    arg_parser = Trainer.add_argparse_args(arg_parser)
-    args = arg_parser.parse_args()
+def main(args: Namespace) -> None:
     data = QuackTokenizedDataModule(args.data_dir, batch_size=args.batch_size, workers=args.num_workers)
     # Max value of static is from the ipv4 segments.
     max_index = 256 + QuackConstants.VOCAB.value
@@ -42,6 +30,7 @@ def main() -> None:
         trainer = Trainer.from_argparse_args(
             args,
             precision=16,
+            strategy='ddp',
             callbacks=[early_stopping_callback, checkpoint_callback]
         )
         print('Ready for training...')
@@ -52,4 +41,16 @@ def main() -> None:
 
 
 if __name__ == '__main__':
-    main()
+    # Add arguments to make a more flexible cli tool.
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument('--data_dir', type=str, default='/data')
+    arg_parser.add_argument('--batch_size', type=int, default=4)
+    arg_parser.add_argument('--num_workers', type=int, default=0)
+    arg_parser.add_argument('--embed_size', type=int, default=128)
+    arg_parser.add_argument('--hidden_size', type=int, default=512)
+    arg_parser.add_argument('--tune', action='store_true', default=False)
+    arg_parser.add_argument('--checkpoint_path', type=str)
+    # add trainer arguments (gpus=x, precision=...)
+    arg_parser = Trainer.add_argparse_args(arg_parser)
+    arguments = arg_parser.parse_args()
+    main(arguments)
