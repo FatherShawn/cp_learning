1bee56ac64c5396902596076450f2248332843bf
Shawn P. Duncan <sduncan@digitalpulp.com>
2022-01-14 21:36:32 -0500

    Trial job script

---
+++ job_scripts/device_query.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --partition interactive
+#SBATCH -J spdDeviceQuery
+#SBATCH --ntasks l
+#SBATCH --gres gpu:1
+#SBATCH --mem <fond color="red">4</fond color>
+
+
+# change to the working directory
+cd $SLURM_WORKDIR
+
+echo ">>>> Begin spdDeviceQuery"
+
+# actual binary (with IO redirections) and required input
+# parameters is called in the next line
+git checkout https://github.com/NVIDIA/cuda-samples.git
+cd cuda-samples/Samples/1_Utilities/deviceQuery/
+make clean
+make
+
