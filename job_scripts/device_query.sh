e43ce0957aa7a6969aaf43c9649ba927df0302ee
Shawn P. Duncan <sduncan@digitalpulp.com>
2022-01-14 22:25:43 -0500

    script typo in tasks

--- job_scripts/device_query.sh
+++ job_scripts/device_query.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition interactive
 #SBATCH -J spdDeviceQuery
-#SBATCH --ntasks l
+#SBATCH --ntasks 1
 #SBATCH --gres gpu:1
 #SBATCH --mem <fond color="red">4</fond color>
 
