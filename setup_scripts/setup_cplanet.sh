611cd3fb31a0e5ed547dde71230fddeb644f4dfc
Shawn P. Duncan <code@sd.shawnduncan.org>
2022-01-23 07:40:53 -0500

    Full feature output to setup_cplanet.sh

--- setup_scripts/setup_cplanet.sh
+++ setup_scripts/setup_cplanet.sh
@@ -2,14 +2,25 @@
 # Setup the environment in the HPC cluster.
 
 # Load modules
+echo "Restore modules"
 module purge
 module restore
+echo "Activate cplanet conda environment"
 conda env create --file /global/u/shawn_bc_10/cplanet.yml
 conda activate cplanet
 # Python now at ~/.conda/envs/cplanet/bin/python
 # These packages are not in conda and would not build with conda-build skeleton:
+echo "Add non-conda packages"
 pip install 'fairseq>=0.10.0'
 pip install 'urlextract>=1.3.0'
 pip install 'webdataset==0.1.62'
 # Pull down project code
-git clone https://github.com/FatherShawn/cp_learning.git
+echo "Clone or update project code"
+if [[ -d /scratch/shawn_bc_10/cp_learning ]]
+then
+    cd /scratch/shawn_bc_10/cp_learning
+    git fetch && git pull
+else
+  cd /scratch/shawn_bc_10
+  git clone https://github.com/FatherShawn/cp_learning.git
+fi
