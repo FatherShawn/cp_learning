b1c0824b8dc991d2fcd58e2e7f2e0870f2113c48
Shawn P. Duncan <code@sd.shawnduncan.org>
2022-01-24 06:29:27 -0500

    encode_data.sh ready for trial

--- job_scripts/encode_data.sh
+++ job_scripts/encode_data.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name="dataEncoder"
 #SBATCH --partition production
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=4
 #SBATCH --gres=gpu:1
-#SBATCH --mem=8GB
+#SBATCH --mem=32GB
 
 
 # change to the working directory
@@ -17,3 +17,7 @@ cp /global/u/shawn_bc_10/encoder_data.tar .
 tar -xf ./encoder_data.tar --checkpoint=.1000
 echo ">> Data ready for the run"
 python ae_processor.py  --encode --filtered --data_dir $(pwd)/pickled --storage_path $(pwd)/encoded --batch_size 6 --num_workers 4 --embed_size 96 --hidden_size 256 --gpus 1 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-178853.ckpt
+echo ">> Data ready for the run"
+tar -cf $(pwd)/encoder_processed.tar $(pwd)//encoded
+rm -r $(pwd)//encoded
+echo ">> Encoded data ready"
