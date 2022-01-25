4baa31037624b9efc611882b17b8be2484ef271c
Shawn P. Duncan <code@sd.shawnduncan.org>
2022-01-25 07:57:07 -0500

    Activate conda environment in job script.

--- job_scripts/test_data.sh
+++ job_scripts/test_data.sh
@@ -11,6 +11,16 @@
 cd $SLURM_WORKDIR
 
 echo ">>>> Begin dataVerifcation"
-
+echo "Activate cplanet conda environment"
+# Running a subshell, we need to run the init hook code directly.
+eval "$(conda shell.bash hook)"
+if { conda env list | grep 'cplanet'; } >/dev/null 2>&1;
+then
+   conda activate cplanet
+   echo "Environment cplanet activated"
+else
+  echo "Environment cplanet not found, exiting :("
+  exit 1
+fi
 # Copy and expand data
 python $(pwd)/cp_learning//dataset_test.py  --encode --filtered --data_dir $(pwd)/pickled
