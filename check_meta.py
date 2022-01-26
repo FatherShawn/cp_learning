6769a97067764e30adeebb405f5a622ec301fb12
Shawn P. Duncan <code@sd.shawnduncan.org>
2022-01-26 06:39:13 -0500

    updated requirements.txt

---
+++ check_meta.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import pickle
+
+# 891069
+
+if __name__ == '__main__':
+    # Add args to make a more flexible cli tool.
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument('--storage_path', type=str, required=True)
+    args = arg_parser.parse_args()
+    with open(args.storage_path + '/metadata.pyc', 'rb') as retrieved_dict:
+        metadata = pickle.load(retrieved_dict)
+    for key, value in metadata.items():
+        print(f'{key}: {value}')
