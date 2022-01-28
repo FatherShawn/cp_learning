c323de017605dc1dddc4c67037d7a3f425ac4841
Shawn P. Duncan <code@sd.shawnduncan.org>
2022-01-28 06:42:22 -0500

    no global file system in compute nodes

--- job_scripts/multi_node_multi_gpu_tune.sh
+++ job_scripts/multi_node_multi_gpu_tune.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name="batchCheck"
 #SBATCH --partition development
-#SBATCH --nodes=8
-#SBATCH --ntasks=8
+#SBATCH --nodes=$(pwd)/archived-checkpoints
+#SBATCH --ntasks=$(pwd)/archived-checkpoints
 #SBATCH --gres=gpu:2
-#SBATCH --mem=8GB
+#SBATCH --mem=$(pwd)/archived-checkpointsGB
 
 # pytorch lighting tune did not correctly report a working batch size for this model, so try several.
 
@@ -14,10 +14,10 @@ cd $SLURM_WORKDIR
 echo ">>>> Begin batchCheck"
 
 echo "############# Batch size 12"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 12 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-178853.ckpt --fast_dev_run 10
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 12 --num_workers $(pwd)/archived-checkpoints --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-17$(pwd)/archived-checkpoints$(pwd)/archived-checkpoints53.ckpt --fast_dev_run 10
 
 echo "############# Batch size 24"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 24 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-178853.ckpt --fast_dev_run 10
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 24 --num_workers $(pwd)/archived-checkpoints --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-17$(pwd)/archived-checkpoints$(pwd)/archived-checkpoints53.ckpt --fast_dev_run 10
 
-echo "############# Batch size 48"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 48 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-178853.ckpt --fast_dev_run 10
+echo "############# Batch size 4$(pwd)/archived-checkpoints"
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 4$(pwd)/archived-checkpoints --num_workers $(pwd)/archived-checkpoints --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-17$(pwd)/archived-checkpoints$(pwd)/archived-checkpoints53.ckpt --fast_dev_run 10
--- job_scripts/single_node_multi_gpu_tune.sh
+++ job_scripts/single_node_multi_gpu_tune.sh
@@ -14,10 +14,10 @@ cd $SLURM_WORKDIR
 echo ">>>> Begin batchCheck"
 
 echo "############# Batch size 12"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 12 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-178853.ckpt --fast_dev_run 10
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 12 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path $(pwd)/archived-checkpoints/epoch-40-step-178853.ckpt --fast_dev_run 10
 
 echo "############# Batch size 24"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 24 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-178853.ckpt --fast_dev_run 10
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 24 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path $(pwd)/archived-checkpoints/epoch-40-step-178853.ckpt --fast_dev_run 10
 
 echo "############# Batch size 48"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 48 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-178853.ckpt --fast_dev_run 10
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --batch_size 48 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 2 --checkpoint_path $(pwd)/archived-checkpoints/epoch-40-step-178853.ckpt --fast_dev_run 10
