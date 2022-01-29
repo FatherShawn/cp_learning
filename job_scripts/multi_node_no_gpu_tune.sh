5b4caf9f5cee8b5a0418530034eeddedeb270f61
Shawn P. Duncan <sduncan@digitalpulp.com>
2022-01-29 13:04:16 -0500

    single batch experiment

--- job_scripts/multi_node_no_gpu_tune.sh
+++ job_scripts/multi_node_no_gpu_tune.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name="n-t-32-256"
+#SBATCH --job-name="18-batch"
 #SBATCH --partition production
-#SBATCH --nodes=24
-#SBATCH --ntasks=192
-#SBATCH --mem=32GBman
+#SBATCH --nodes=16
+#SBATCH --ntasks=128
+#SBATCH --mem=32Gb
 
 # pytorch lighting tune did not correctly report a working batch size for this model, so try several.
 
 # change to the working directory
 cd $SLURM_WORKDIR
 
-echo ">>>> Begin batch-18-24-28"
+echo ">>>> Begin batch 18"
 
-echo "############# Batch size 18"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 24 --batch_size 18 --num_workers 8 --embed_size 96 --hidden_size 256 --fast_dev_run 10
-
-echo "############# Batch size 24"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 24 --batch_size 24 --num_workers 8 --embed_size 96 --hidden_size 256 --fast_dev_run 10
-
-echo "############# Batch size 28"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 24 --batch_size 28 --num_workers 8 --embed_size 96 --hidden_size 256 --fast_dev_run 10
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 16 --batch_size 18 --num_workers 7 --embed_size 96 --hidden_size 256 --fast_dev_run 10
