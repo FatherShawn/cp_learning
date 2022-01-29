54132af7f7db8536173323b29bd08c0fd4e9431e
Shawn P. Duncan <code@sd.shawnduncan.org>
2022-01-29 12:15:28 -0500

    experimenting with batches v2

--- job_scripts/ae_processing.sh
+++ job_scripts/ae_processing.sh
@@ -12,4 +12,4 @@ cd $SLURM_WORKDIR
 
 echo ">>>> Begin autoEncoderTraining"
 
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage --batch_size 12 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 8 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-178853.ckpt
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage --batch_size 12 --num_workers 8 --embed_size 96 --hidden_size 256 --limit_train_batches 10 --limit_val_batches 10 --checkpoint_path /global/u/shawn_bc_10/checkpoints/epoch-40-step-178853.ckpt
--- job_scripts/multi_node_no_gpu_tune.sh
+++ job_scripts/multi_node_no_gpu_tune.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name="batch-0-16-128-16"
-#SBATCH --partition development
-#SBATCH --nodes=16
-#SBATCH --ntasks=128
-#SBATCH --mem=32GB
+#SBATCH --job-name="n-t-32-256"
+#SBATCH --partition production
+#SBATCH --nodes=24
+#SBATCH --ntasks=192
+#SBATCH --mem=32GBman
 
 # pytorch lighting tune did not correctly report a working batch size for this model, so try several.
 
 # change to the working directory
 cd $SLURM_WORKDIR
 
-echo ">>>> Begin batch-0-16-128-16"
+echo ">>>> Begin batch-18-24-28"
 
-echo "############# Batch size 6"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 16 --batch_size 6 --num_workers 8 --embed_size 96 --hidden_size 256 --fast_dev_run 10
+echo "############# Batch size 18"
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 24 --batch_size 18 --num_workers 8 --embed_size 96 --hidden_size 256 --fast_dev_run 10
 
-echo "############# Batch size 8"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 16 --batch_size 8 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 1 --fast_dev_run 10
+echo "############# Batch size 24"
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 24 --batch_size 24 --num_workers 8 --embed_size 96 --hidden_size 256 --fast_dev_run 10
 
-echo "############# Batch size 12"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 16 --batch_size 12 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 1 --fast_dev_run 10
+echo "############# Batch size 28"
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 24 --batch_size 28 --num_workers 8 --embed_size 96 --hidden_size 256 --fast_dev_run 10
