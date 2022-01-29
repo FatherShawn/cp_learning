3f098e76decd007b7d9a5080f409426ef8a41e10
Shawn P. Duncan <code@sd.shawnduncan.org>
2022-01-29 07:24:10 -0500

    pass num_nodes to lightning

--- job_scripts/multi_node_no_gpu_tune.sh
+++ job_scripts/multi_node_no_gpu_tune.sh
@@ -13,10 +13,10 @@ cd $SLURM_WORKDIR
 echo ">>>> Begin batch-0-16-128-16"
 
 echo "############# Batch size 6"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --batch_size 6 --num_workers 8 --embed_size 96 --hidden_size 256 --fast_dev_run 10
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 16 --batch_size 6 --num_workers 8 --embed_size 96 --hidden_size 256 --fast_dev_run 10
 
 echo "############# Batch size 8"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --batch_size 8 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 1 --fast_dev_run 10
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 16 --batch_size 8 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 1 --fast_dev_run 10
 
 echo "############# Batch size 12"
-python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --batch_size 12 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 1 --fast_dev_run 10
+python $(pwd)/cp_learning/ae_processor.py  --data_dir $(pwd)/pickled --comet_storage $(pwd)/comet_storage_tune --accelerator cpu --num_nodes 16 --batch_size 12 --num_workers 8 --embed_size 96 --hidden_size 256 --gpus 1 --fast_dev_run 10
