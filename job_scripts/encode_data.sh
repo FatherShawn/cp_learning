c65bc2afd33b24f3416b587a97fdb540f04028e7
Shawn P. Duncan <code@sd.shawnduncan.org>
2022-03-01 07:24:02 -0500

    update needs in encode_data.sh

--- job_scripts/encode_data.sh
+++ job_scripts/encode_data.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition production
 #SBATCH --nodes=1
 #SBATCH --ntasks=16
-#SBATCH --mem=64GB
+#SBATCH --mem=44GB
 
 
 # change to the working directory
@@ -11,4 +11,4 @@ cd $SLURM_WORKDIR
 
 echo ">>>> Begin dataEncoder"
 
-python $(pwd)/cp_learning/ae_processor.py  --encode --filtered --data_dir $(pwd)/pickled --storage_path $(pwd)/encoded --batch_size 6 --num_workers 15 --embed_size 96 --hidden_size 256 --checkpoint_path $(pwd)/auto_checkpoints/autoencoder_epoch__02-step__15999-val_loss__222.94.ckpt
+python $(pwd)/cp_learning/ae_processor.py  --encode --filtered --data_dir $(pwd)/pickled --storage_path $(pwd)/encoded --batch_size 6 --num_workers 15 --embed_size 96 --hidden_size 256 --checkpoint_path $(pwd)/archived-checkpoints/autoencoder_epoch__02-step__15999-val_loss__222.94.ckpt
